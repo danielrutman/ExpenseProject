43f77b8509f7fb876296f31015201b1f56212c00
daniel.rutman <danielrutman8@gmail.com>
2026-05-24 13:16:03 +0300

    Add Terraform IaC: ECR, IAM, Lambda, API Gateway
    - main.tf: provider config + API Gateway URL output
    - resources.tf: all AWS resources
    - .gitignore: exclude state files and .terraform directory

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+#terraform config file to be used by terraform to config infrastruct to desired state
+
+#declaration of needed provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+#initialization of providers
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# Output the API Gateway URL
+output "api_gateway_url" {
+  value = "${aws_apigatewayv2_stage.default.invoke_url}/expense-bot"
+  description = "Twilio webhook URL"
+}
+
---
+++ terraform/resources.tf
@@ -0,0 +1,121 @@
+#terrafrom resource file for expense project
+
+# ECR repository for expense-bot Docker image
+resource "aws_ecr_repository" "expense_bot" {
+  name         = "expense-bot"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = false
+  }
+}
+
+# IAM policy for GitHub Actions
+resource "aws_iam_policy" "github_actions_policy" {
+  name = "github-actions-expense-bot-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:PutImage"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = ["lambda:UpdateFunctionCode"]
+        Resource = "arn:aws:lambda:eu-central-1:115643029932:function:expense-bot"
+      }
+    ]
+  })
+}
+
+# IAM user for GitHub Actions
+resource "aws_iam_user" "github_actions_user" {
+  name = "github-actions-expense-bot"
+}
+
+# Attach policy to user
+resource "aws_iam_user_policy_attachment" "github_actions_attachment" {
+  user       = aws_iam_user.github_actions_user.name
+  policy_arn = aws_iam_policy.github_actions_policy.arn
+}
+
+# IAM role for Lambda execution
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "expense-bot-lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = { Service = "lambda.amazonaws.com" }
+        Action    = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Attach basic Lambda execution policy (CloudWatch logs)
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# Lambda function
+resource "aws_lambda_function" "expense_bot" {
+  function_name = "expense-bot"
+  role          = aws_iam_role.lambda_exec_role.arn
+  package_type  = "Image"
+  image_uri     = "${aws_ecr_repository.expense_bot.repository_url}:latest"
+  memory_size   = 256
+  timeout       = 30
+}
+
+# API Gateway HTTP API
+resource "aws_apigatewayv2_api" "expense_bot" {
+  name          = "expense-bot-API"
+  protocol_type = "HTTP"
+}
+
+# API Gateway stage
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.expense_bot.id
+  name        = "default"
+  auto_deploy = true
+}
+
+# Lambda integration
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id             = aws_apigatewayv2_api.expense_bot.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.expense_bot.invoke_arn
+  payload_format_version = "2.0"
+}
+
+# Route
+resource "aws_apigatewayv2_route" "post" {
+  api_id    = aws_apigatewayv2_api.expense_bot.id
+  route_key = "POST /expense-bot"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+# Permission for API Gateway to invoke Lambda
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.expense_bot.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.expense_bot.execution_arn}/*/*"
+}
